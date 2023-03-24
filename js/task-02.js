999b754637652c429f1bb5e976a228c1ffb8aa43
Kurnosov Valentyn <deyke66@gmail.com>
2023-03-24 19:18:19 +0100

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ulRef = document.querySelector('#ingredients');
+
+const newMarkup = ingredients.map(item => {
+  const li = document.createElement('li');
+  li.textContent = item
+  li.classList.add('item')
+  return li
+});
+
+ulRef.append(...newMarkup);
