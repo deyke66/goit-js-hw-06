a7820e0b0d1eb7115d4161e6cfd1c8ad8aa82829
Kurnosov Valentyn <deyke66@gmail.com>
2023-03-26 15:46:43 +0200

    task 6 done

---
+++ js/task-06.js
@@ -0,0 +1,3 @@
+const inputRef = document.querySelector('#validation-input');
+
+inputRef.addEventListener('blur', event => inputRef.dataset.length > event.currentTarget.value.length ? inputRef.classList.add('invalid') : inputRef.classList.add('valid'))
