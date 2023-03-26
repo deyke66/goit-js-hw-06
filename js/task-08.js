292221700310bc09ebfae962ef59c51afa4d9295
Kurnosov Valentyn <deyke66@gmail.com>
2023-03-26 18:14:32 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const dataForm = document.querySelector('.login-form');
 
+dataForm.addEventListener('submit', onSubmit)
+
+function onSubmit(event) {
+    event.preventDefault()
+
+    const { email, password } = event.currentTarget.elements;
+    
+    const userData = {
+        email: email.value,
+        password: password.value
+    }
+    console.log(userData)
+};
