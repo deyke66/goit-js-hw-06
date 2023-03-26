73569dd709f5e27afd3d13430bf618990b15f95b
Kurnosov Valentyn <deyke66@gmail.com>
2023-03-26 18:41:44 +0200

    Доробив 8 завдання

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,22 @@
 const dataForm = document.querySelector('.login-form');
 
-dataForm.addEventListener('submit', onSubmit)
+dataForm.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
     event.preventDefault()
-
+   
     const { email, password } = event.currentTarget.elements;
-    
-    const userData = {
-        email: email.value,
-        password: password.value
-    }
-    console.log(userData)
-};
+
+    if (email.value === '' || password.value === '') {
+        alert('Заповніть всі поля вводу')
+    } else {
+        const userData = {
+                email: email.value,
+                password: password.value
+        }
+
+        console.log(userData)
+
+        dataForm.reset()
+    }   
+}
