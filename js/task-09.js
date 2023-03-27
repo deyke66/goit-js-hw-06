c6d6c497acc358271a9cc33c77011bf9700918cf
Kurnosov Valentyn <deyke66@gmail.com>
2023-03-27 12:46:45 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const mainColorValue = document.querySelector('body');
+
+const changeColorButton = document.querySelector('.change-color');
+
+const nameColorText = document.querySelector('.color');
+
+changeColorButton.addEventListener('click', onChangeColorButton);
+
+function onChangeColorButton(event) {
+  const colorValue = getRandomHexColor();
+  const nameColor = colorValue;
+  return mainColorValue.style.backgroundColor = colorValue, nameColorText.textContent = nameColor;
+}
